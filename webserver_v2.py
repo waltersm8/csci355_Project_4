26d3ed26871e783015516852b6b71ca5d2188215
MaxFerney <ferneym2@winthrop.edu>
2019-11-22 15:20:29 -0500

    attempt to GET index

--- webserver_v2.py
+++ webserver_v2.py
@@ -8,7 +8,9 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b'Hello, world!')
+        # self.wfile.write(b'Hello, world!')
+        if self.path == '/':
+            self.path == '/index.html'
 
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
