83b034e9db0097831fa25ef5f1679a91787f2a32
MaxFerney <46454102+MaxFerney@users.noreply.github.com>
2019-12-01 18:27:02 -0500

    Changed database to main database.

--- mysqlTest.py
+++ mysqlTest.py
@@ -3,41 +3,24 @@ import mysql.connector
 import time
 
 
-mydb = mysql.connector.connect(
-  host="deltona.birdnest.org",
-  user="my.ferneym2",
-  passwd="max1well",
-  database="my_ferneym2_355_p4_test"
-)
-
-
-# #Actual Database    
 # mydb = mysql.connector.connect(
 #   host="deltona.birdnest.org",
-#   user="my.waltersm8",
-#   passwd="Swa3ugus",
-#   database="my_waltersm8_csci355Project4"
+#   user="my.ferneym2",
+#   passwd="max1well",
+#   database="my_ferneym2_355_p4_test"
 # )
 
+
+#Actual Database    
+mydb = mysql.connector.connect(
+  host="deltona.birdnest.org",
+  user="my.waltersm8",
+  passwd="Swa3ugus",
+  database="my_waltersm8_csci355Project4"
+)
+
 print(mydb)
-
 mycursor = mydb.cursor()
-print(mycursor)
-
-print("===Showing Databases===")
-mycursor.execute("SHOW DATABASES")
-
-for x in mycursor:
-    print(x)
-
-print("===Showing Tables===")
-mycursor.execute("SHOW TABLES")
-
-myresult = mycursor.fetchall()
-for x in myresult:
-  print(x)
-
-
 
 def main():
   while True:
