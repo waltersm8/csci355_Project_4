10ae1d7c6480e4c185998f6b2abf98d2422e78b8
MaxFerney <46454102+MaxFerney@users.noreply.github.com>
2019-12-01 17:16:33 -0500

    Added USER data

--- seed.sql
+++ seed.sql
@@ -5,3 +5,6 @@ INSERT INTO DEVELOPER (name) VALUES ("Example Developer 2");
 INSERT INTO GAME (title, developer, genre, ageLimit) VALUES ("Example Game", 1, 1, 18);
 INSERT INTO GAME (title, developer, genre) VALUES ("Example Puzzle Game", 2, 2);
 INSERT INTO GAME (title, developer, genre) VALUES ("Example Action Game", 1, 2);
+INSERT INTO USER (userName, fName, lName) VALUES ("bdawg, Andrew, Besmer");
+INSERT INTO USER (userName, fName, lName, birthdate) VALUES ("Dovah, Max, Ferney, 1997-05-23");
+INSERT INTO USER (userName, fName) VALUES ("Mezna, Michael");
