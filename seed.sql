5bc0252d53c50b52ac4f994b2945aabf6cc15ede
MaxFerney <46454102+MaxFerney@users.noreply.github.com>
2019-12-01 18:08:46 -0500

    fixed quotations

--- seed.sql
+++ seed.sql
@@ -6,5 +6,5 @@ INSERT INTO GAME (title, developer, genre, ageLimit) VALUES ("Example Game", 1, 
 INSERT INTO GAME (title, developer, genre) VALUES ("Example Puzzle Game", 2, 2);
 INSERT INTO GAME (title, developer, genre) VALUES ("Example Action Game", 1, 2);
 INSERT INTO USER (userName, fName, lName) VALUES ("bdawg, Andrew, Besmer");
-INSERT INTO USER (userName, fName, lName, birthdate) VALUES ("Dovah, Max, Ferney, 1997-05-23");
-INSERT INTO USER (userName, fName) VALUES ("Mezna, Michael");
+INSERT INTO USER (userName, fName, lName, birthdate) VALUES ("Dovah", "Max", "Ferney", 1997-05-23);
+INSERT INTO USER (userName, fName) VALUES ("Mezna", "Michael");
